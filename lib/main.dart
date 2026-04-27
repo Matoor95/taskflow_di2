6d3865cba56fa990b707878c5b4c2cf8906b9dc0
Matoor95 <seckmatar9521@gmail.com>
2026-04-27 10:17:43 +0100

    design login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:taskflow_di2/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'firebase_options.dart';
+import 'screens/auth/login_page.dart';
+import 'screens/home/home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Initialisation Firebase
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -16,49 +18,54 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'TaskFlow',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepOrange,
+          brightness: Brightness.light,
+        ),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepOrange,
+          brightness: Brightness.dark,
+        ),
+        useMaterial3: true,
+      ),
+      themeMode: ThemeMode.system,
+      home: const AuthWrapper(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class AuthWrapper extends StatelessWidget {
+  const AuthWrapper({super.key});
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-        appBar: AppBar(
-          // TRY THIS: Try changing the color here to a specific color (to
-          // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-          // change color while the other colors stay the same.
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Text(widget.title),
-        ),
-        body: Center(
-          child: Text('welcome'),
-        ));
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        // Firebase en cours d'initialisation
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        }
+
+        // Utilisateur connecté → HomePage
+        if (snapshot.hasData) {
+          return const HomePage();
+        }
+
+        // Utilisateur non connecté → LoginPage
+        return const LoginPage();
+      },
+    );
   }
 }
--- lib/screens/auth/login_page.dart
+++ lib/screens/auth/login_page.dart
@@ -1,77 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:taskflow_di2/screens/auth/widgets/auth_text_field.dart';
 import 'package:taskflow_di2/services/auth/auth_service.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({ Key? key }) : super(key: key);
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
-  final _formKey=GlobalKey<FormState>();
+  final _formKey = GlobalKey<FormState>();
   final _emailController = TextEditingController();
-  final _passwordController=TextEditingController();
-  final _authService=AuthService();
-  bool _loading=false;
+  final _passwordController = TextEditingController();
+  final _authService = AuthService();
+  bool _loading = false;
   @override
-  // dispose permet de nettoyer les resource , il est essentiel a la gestioon de la memoire 
-  void dispose (){
+  // dispose permet de nettoyer les resource , il est essentiel a la gestioon de la memoire
+  void dispose() {
     _emailController.dispose();
     _passwordController.dispose();
     super.dispose();
-    
   }
 
-  Future <void> _login() async {
-    if(!_formKey.currentState!.validate()) return ;
-    setState(()=>_loading=true);
-    final error=await _authService.login(
-      email:_emailController.text,
-      password:_passwordController.text,
+  Future<void> _login() async {
+    if (!_formKey.currentState!.validate()) return;
+    setState(() => _loading = true);
+    final error = await _authService.login(
+      email: _emailController.text,
+      password: _passwordController.text,
     );
-    //si le widget n'est plus dansd l'arbre 
-    if(!mounted) return ;
-    setState(()=>_loading=false);
-    if(error != null){
+    //si le widget n'est plus dansd l'arbre
+    if (!mounted) return;
+    setState(() => _loading = false);
+    if (error != null) {
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content:Text(error), backgroundColor:const Color (0xFFEF4444)),
+        SnackBar(
+            content: Text(error), backgroundColor: const Color(0xFFEF4444)),
       );
-
     }
-
   }
- 
-  Future <void> _forgotPassword() async{
-    if(_emailController.text.isEmpty){
+
+  Future<void> _forgotPassword() async {
+    if (_emailController.text.isEmpty) {
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text("Entrez votre email d\'abord"))
-      );
+          const SnackBar(content: Text("Entrez votre email d\'abord")));
       return;
-
     }
-    final error= await _authService.sendPasswordReset(_emailController.text);
-    if(!mounted) return ;
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text(error ?? 'Email de renitialisation envoye'),
-        backgroundColor: error != null ? const Color(0xFFEF4444) : const Color(0xFFfE444),
-        )
-    );
+    final error = await _authService.sendPasswordReset(_emailController.text);
+    if (!mounted) return;
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(error ?? 'Email de renitialisation envoye'),
+      backgroundColor:
+          error != null ? const Color(0xFFEF4444) : const Color(0xFFfE444),
+    ));
   }
+
   Widget build(BuildContext context) {
- const indigo = Color(0xFF4F46E5);
+    const indigo = Color(0xFF4F46E5);
     const indigoLight = Color(0xFFEEF2FF);
     const textPrimary = Color(0xFF111827);
     const textSecondary = Color(0xFF6B7280);
     const borderColor = Color(0xFFE5E7EB);
-        return  Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Center(
-          child: SingleChildScrollView(
-            padding: EdgeInsets.symmetric(horizontal: 32, vertical: 24),
-            child: Form(
+          child: Center(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(horizontal: 32, vertical: 24),
+          child: Form(
               key: _formKey,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -81,21 +78,73 @@ class _LoginPageState extends State<LoginPage> {
                     width: 64,
                     height: 64,
                     decoration: BoxDecoration(
-                      color: indigoLight,
-                      borderRadius: BorderRadius.circular(16)
-                    ),
-                    child: const Icon(Icons.bolt_rounded, size: 36, color: indigo),
+                        color: indigoLight,
+                        borderRadius: BorderRadius.circular(16)),
+                    child:
+                        const Icon(Icons.bolt_rounded, size: 36, color: indigo),
+                  ),
+                  const SizedBox(
+                    height: 24.0,
+                  ),
+                  // titre
+                  const Text(
+                    'Bon Retour',
+                    style: TextStyle(
+                        fontSize: 26,
+                        color: textSecondary,
+                        fontWeight: FontWeight.w700),
+                  ),
+                  const SizedBox(
+                    height: 6,
+                  ),
+                  const Text('connectez-vous a voitre espace Taskflow',
+                      style: TextStyle(
+                          fontSize: 13,
+                          fontWeight: FontWeight.w600,
+                          color: textPrimary)),
+                  const SizedBox(
+                    height: 6,
+                  ),
+                  // const Text('Adrese email', style: TextStyle(fontSize: 13, fontWeight: FontWeight.w600, color: textPrimary),),
+                  AuthTextField(
+                    controller: _emailController,
+                    hint: 'email@gmail.com',
+                    label: 'votre email',
+                    prefixIcon: Icons.email_outlined,
+                    validator: (v) => (v == null || !v.contains('@'))
+                        ? 'email invalide'
+                        : null,
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  // label du mot de passe
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        'Mot de passe',
+                        style: TextStyle(
+                            fontSize: 13,
+                            fontWeight: FontWeight.w600,
+                            color: textPrimary),
+                      ),
+                      GestureDetector(
+                        onTap: _forgotPassword,
+                        child: const Text(
+                          'Mot de passe opublie ?',
+                          style: TextStyle(
+                              fontSize: 13,
+                              fontWeight: FontWeight.w600,
+                              color: indigo),
+                        ),
+                      )
+                    ],
                   )
                 ],
-              )
-              ),
-
-          ),
-
-        )
-        )
-        ,
-
+              )),
+        ),
+      )),
     );
   }
 }
